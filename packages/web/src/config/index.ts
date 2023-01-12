1d3fdc8657647f03f78c2c4429b22811dd97610a
JordanAmaralVicente <jordanitalovicente@gmail.com>
2023-01-12 10:37:25 -0300

    feat(web/config): update server address

--- packages/web/src/config/index.ts
+++ packages/web/src/config/index.ts
@@ -7,7 +7,13 @@ interface AppConfig {
 
 export const appConfig: AppConfig = {
   api: {
-    url: "http://localhost:8080/api",
-    websocketUrl: "ws://localhost:8081",
+    url:
+      process.env.NODE_ENV === "production"
+        ? "http://54.237.12.36:8080/api"
+        : "http://localhost:8080/api",
+    websocketUrl:
+      process.env.NODE_ENV === "production"
+        ? "ws://54.237.12.36:8081"
+        : "ws://localhost:8081",
   },
 };
