cccc715d14a6599e106cf82d48d95a18dcbc10c4
JordanAmaralVicente <jordanitalovicente@gmail.com>
2023-01-12 09:33:17 -0300

    chore(server): update web port

--- packages/server/src/middlewares/cors.ts
+++ packages/server/src/middlewares/cors.ts
@@ -1,6 +1,6 @@
 import cors from "cors";
 
 export const corsMiddleware = cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "http://localhost:80"],
     methods: ["HEAD", "OPTIONS", "PUT", "PATCH", "POST", "GET"],
 });
