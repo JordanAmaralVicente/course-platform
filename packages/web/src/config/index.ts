a77510e39abc901b8a85ccfbb089ca935bbca1fe
JordanAmaralVicente <jordanitalovicente@gmail.com>
2023-01-13 19:36:27 -0300

    feat(all): change server address

--- packages/web/src/config/index.ts
+++ packages/web/src/config/index.ts
@@ -9,11 +9,11 @@ export const appConfig: AppConfig = {
   api: {
     url:
       process.env.NODE_ENV === "production"
-        ? "http://54.237.12.36:8080/api"
+        ? "http://34.207.222.57:8080/api"
         : "http://localhost:8080/api",
     websocketUrl:
       process.env.NODE_ENV === "production"
-        ? "ws://54.237.12.36:8081"
+        ? "ws://34.207.222.57:8081"
         : "ws://localhost:8081",
   },
 };
